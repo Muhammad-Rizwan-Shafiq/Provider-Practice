681cd31530699bfcf8e3326f819ba008bf2330f3
Muhammad-Rizwan-Shafiq <muhammadrizwanshafiq@gmail.com>
2022-10-31 18:07:10 +0500

    Shoe shi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:providerpractice/custom_bottom_navigation.dart';
 import 'dashboard_screen.dart';
 
@@ -12,12 +13,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CustomBottomNavigation(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+
+          // You can use the library anywhere in the app even in theme
+
+          home: child,
+
+        );
+      },
+      // child: TestScreen(),
+      child: const CustomBottomNavigation(),
     );
   }
 }
